4904145d518d790b7be0cec0f4f9af4792671422
waashii <70151834+waashii@users.noreply.github.com>
2022-04-05 18:43:59 +0300

    dadada not donu

---
+++ ScienceNlogic/ScienceNlogic/QuiZtry3.swift
@@ -0,0 +1,41 @@
+//
+//  QuiZtry3.swift
+//  ScienceNlogic
+//
+//  Created by washi alhammadi on 05/04/2022.
+//
+
+import SwiftUI
+
+struct QuiZtry3: View {
+    @State var score = 0
+    
+    var body: some View {
+        NavigationView{
+            VStack(spacing: 20){
+                
+                Text("Welcome to the quiz game")
+                
+                //button to start the quiz//
+    NavigationLink(destination: Quiz2()){
+                    Text("start Quiz")
+                }
+                HStack{
+Text("last score :\(self.score)/ \(myQuiz2.count)")
+                        .onAppear(){
+        self.score = LoadScore(quiz: "myQuiz2")
+                        }
+                }
+            }
+            .navigationBarTitle("Quiz example",displayMode: .inline)
+            .navigationBarHidden(true)
+        } .navigationBarHidden(true)
+    }
+}
+
+
+struct QuiZtry3_Previews: PreviewProvider {
+    static var previews: some View {
+        QuiZtry3()
+    }
+}
